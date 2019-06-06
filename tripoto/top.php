17afcf1a77208bfbed7ffb75234261d2651c3ab3
Rajat Gupta <580lpsac@gmail.com>
2019-06-06 22:03:45 +0530

    tripoto

---
+++ tripoto/top.php
@@ -0,0 +1,25 @@
+	 <div class="header-top">
+		 <!--container-->
+		  <div class="container">
+			 <div class="top-nav">
+						<div class="logo">
+						<a href="#"><img src="images/images_152x44.png" id="section-1" class="img-responsive" alt=""/></a>
+						</div>
+						<div class="menu">
+						<ul id="nav">
+							 <li><a href="index.php#section-1"  onclick="javascript:window.open('index.php#section-1','_self')">                            Inspirations</a></li>
+							 <li><a href="index.php#section-2"  onclick="javascript:window.open('index.php#section-2','_self')">                           Forum</a></li>
+							 <li><a href="index.php#section-3"  onclick="javascript:window.open('index.php#section-3','_self')">                             Hotels</a></li>
+                        <li><a href="category.php" onclick="javascript:window.open('category.php','_self')">Category</a></li>                             
+							 <li><a href="index.php#section-4"   onclick="javascript:window.open('index.php#section-4','_self')">                            Holidays</a></li>
+							 <li><a href="index.php#section-5"   onclick="javascript:window.open('index.php#section-5','_self')">                              Publish Trip</a></li>
+							 <div class="clearfix"></div>
+						 </ul>
+						 </div>
+			 </div>
+			  <div class="clearfix"> </div>
+			
+
+		 </div>
+		 <!--/container-->
+	 </div>
